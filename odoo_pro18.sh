16abaead1375c06ac8c78f5850755340d0dce1b8
Haythemvip <info@falcon-v.com>
2020-03-21 03:11:33 -0700

    Update odoo_pro18.sh

--- odoo_pro18.sh
+++ odoo_pro18.sh
@@ -150,10 +150,7 @@ sudo systemctl start odoo
 echo "----------------------------NGINX-------------------------------"
 wget https://raw.githubusercontent.com/FalconValley/OdooScripts/13/nginx.sh
 bash nginx.sh
-echo "---------------------------webmin--------------------------------"
-apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.941_all.deb
-sudo gdebi webmin_1.941_all.deb
+
 
 echo "-----------------------------------------------------------"
 echo "Done! The Odoo production platform is ready:"
